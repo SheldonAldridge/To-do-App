359e74e0c5a6d53844293c216d2bca073dd642a9
Sheldon <sheldonaldridge@yahoo.com>
2022-04-18 11:43:36 +0200

    Update tasklist.js

--- js/tasklist.js
+++ js/tasklist.js
@@ -1,104 +1,129 @@
-window.addEventListener('load', () => {
-	const form = document.querySelector("#new-task-form");
-	const input = document.querySelector("#new-task-input");
-	const list_el = document.querySelector("#tasks");
-	const cal_el = document.querySelector("#calendar");
+/************************************
+ * creates objct of elements needed *
+ ************************************/
+ 
+ const elements = {
+	form: document.querySelector("#new-task-form"),
+	input: document.querySelector("#new-task-input"),
+	list: document.querySelector("#tasks"),
+	cal: document.querySelector("#calendar")
+}
 
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
+/****************************
+ * Generates an ID for task *
+ ****************************/
 
-		const task = input.value;
-		const date = cal_el.value;
+ const createId = () => `${Math.floor(Math.random() * 10000)}-${new Date().getTime()}`
+
+/**********************************************
+ * function that creates the HTML elements    *
+ **********************************************/
+
+ const createTask = () => {
+    const id = createId()
+    const task = elements.input.value;
+    const date = elements.cal.value;
+
+    if(!task && !date) return alert("Please fill in task and select date");
+    if(!task) return alert("Please fill in task");
+    if(!date) return alert("Please select date");
+
+	const tasks = document.createElement("div");
+
+    tasks.innerHTML = `
+    <button class = "sort">Sort</button>
+    <div class="task" data-id = "${id}">
+        <div class="content">
+            <input type ="checkbox" class="tick">
+            <input type ="text" class = "text" id = "text" value="${task}" readonly>
+            <label class = "due-date" for ="text">${date}</label>
+            <input type ="date" class = "date" id = "date">
+        </div>
+
+        <div class = "actions">
+            <button class="edit" data-id="${id}">Edit</button>
+            <button class="delete" data-id="${id}">Delete</button>
+        </div>
+    </div>
+    `
+    elements.list.appendChild(tasks)
+    listen()
+    return tasks
+
+}
 
 
-		if(!task && !date)
-		{
-			alert("Please fill in task and select date")
-			return
-			
-		}
-		if(!task)
-		{
-			alert("Please fill in task")
-			return
-		}
-		if(!date)
-		{
-			alert("Please fill select date")
-			return
-		}
+/********************************************
+ * Marks tasks as complete with checkbox    *
+ ********************************************/
 
-		const task_el = document.createElement('div');
-		task_el.classList.add('task');
+function listen(){
+    let allCheckboxes = document.querySelectorAll('.tick')
+    allCheckboxes.forEach(checkbox =>{
+        checkbox.addEventListener('change',(e)=>{
+            let parentElem=e.target.parentElement
 
-		const task_content_el = document.createElement('div');
-		task_content_el.classList.add('content');
-
-		task_el.appendChild(task_content_el);
-
-		const task_input_el = document.createElement('input');
-		task_input_el.classList.add('text');
-		task_input_el.type = 'text';
-		task_input_el.value = task;
-		task_input_el.setAttribute('readonly', 'readonly');
-
-		task_content_el.appendChild(task_input_el);	
-		
-		const checkbox = document.createElement('input')
-		checkbox.type = "checkbox";
-		checkbox.classList.add('tick')
-
-		task_content_el.appendChild(checkbox);
-	
-		console.log(checkbox);
-
-		
-
-		const due_date = document.createElement('label');
-		due_date.htmlFor = "text";
-		due_date.classList.add('dueDate');
-		due_date.innerText = cal_el.value;
-
-		task_content_el.appendChild(due_date);
-
-		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
-		
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit');
-		task_edit_el.innerText = 'Edit';
-
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete');
-		task_delete_el.innerText = 'Delete';
-
-		task_actions_el.appendChild(task_edit_el);
-		task_actions_el.appendChild(task_delete_el);
-
-		task_el.appendChild(task_actions_el);
-
-		list_el.appendChild(task_el);
-
-		input.value = '';
-
-        task_edit_el.addEventListener('click', () =>{
-            if(task_edit_el.innerText.toLowerCase() == "edit")
-            {
-            task_input_el.removeAttribute("readonly");
-            task_input_el.focus();
-            task_edit_el.innerText = "Save";
+            if(e.target.checked){
+                parentElem.style.textDecoration = "line-through"
             }
-            else
-            {
-                task_input_el.setAttribute("readonly","readonly");
-                task_edit_el.innerText = "Edit";
+            else{
+                parentElem.style.textDecoration = "none"
             }
         });
-
-        task_delete_el.addEventListener('click', () => {
-            list_el.removeChild(task_el);
-        });
-		
     });
+}
 
+/**************************************************************
+ * Event that listens for the edit,save and delete buttons    *
+ **************************************************************/
+elements.list.addEventListener('click',event => {
+    const {target} = event;
+    const {id} = target.dataset;
+    const task = id ? document.querySelector(`[data-id="${id}"]`):null;  
+
+    const type = {
+        edit: event.target.classList.contains('edit'),
+        delete: event.target.classList.contains('delete')
+    }
+
+    const isFromSaveLabel = target.innerText.toLowerCase() === 'save'
+
+    //Checking to see if buttons are pressed
+
+    if(task && type.edit && isFromSaveLabel){
+        const text = task.querySelector('text')
+        target.innerText = 'Edit'
+        text.setAttribute('readonly', true)
+        return
+    };
+
+    if(task && type.edit){
+        const text = task.querySelector('text')
+        target.innerText = 'save'
+        text.removeAttribute('readonly')
+        text.focus()
+        return
+    };
+
+    if(task && type.delete){
+        task.querySelector('.task')
+		elements.list.removeChild(task)
+        return
+    }
+    
 });
+
+/*******************************************************************
+ * Submits the HTML elements to have the lists submited and created*
+ *******************************************************************/
+
+ const submitHandler = (event) =>{
+    event.preventDefault();
+    createTask();
+}
+
+
+elements.form.addEventListener("submit", submitHandler);
+
+
+
